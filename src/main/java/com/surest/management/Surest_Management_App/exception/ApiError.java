295ac19c43c0a64a84916497eabf9d03163828ca
pyankur <40889261+pyankur@users.noreply.github.com>
2025-11-24 15:55:09 +0530

    Revert "Initial Spring Boot project commit"
    This reverts commit 63297d9f5f594de9a36f97236e5c7e6fe4a7c310.

--- src/main/java/com/surest/management/Surest_Management_App/exception/ApiError.java
+++
@@ -1,9 +0,0 @@
-package com.surest.management.Surest_Management_App.exception;
-
-import java.time.OffsetDateTime;
-
-public class ApiError {
-    public OffsetDateTime timestamp = OffsetDateTime.now();
-    public int status;
-    public String message;
-}
--- src/test/java/com/surest/management/Surest_Management_App/controller/AuthControllerTest.java
+++
@@ -1,4 +0,0 @@
-package com.surest.management.Surest_Management_App.controller;
-
-public class AuthControllerTest {
-}
